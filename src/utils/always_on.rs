5a1dd9e76983bc6f0b5ca69e64b71d98d61efea8
Sascha Montellese <sascha.montellese@gmail.com>
2021-09-22 22:27:18 +0200

    + extend AlwaysOn trait with (re)set_always_on()

--- src/utils/always_on.rs
+++ src/utils/always_on.rs
@@ -4,4 +4,6 @@ use mockall::automock;
 #[cfg_attr(test, automock)]
 pub trait AlwaysOn: Send {
     fn is_always_on(&self) -> bool;
+    fn set_always_on(&self) -> Result<(), Box<dyn std::error::Error>>;
+    fn reset_always_on(&self) -> Result<(), Box<dyn std::error::Error>>;
 }
--- src/utils/always_on_file.rs
+++ src/utils/always_on_file.rs
@@ -20,10 +20,100 @@ impl AlwaysOn for AlwaysOnFile {
     fn is_always_on(&self) -> bool {
         self.file.exists()
     }
+
+    fn set_always_on(&self) -> Result<(), Box<dyn std::error::Error>> {
+        match std::fs::write(&self.file, "") {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+
+    fn reset_always_on(&self) -> Result<(), Box<dyn std::error::Error>> {
+        match std::fs::remove_file(&self.file) {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                use std::io::ErrorKind::*;
+                match e.kind() {
+                    // it's OK if the file didn't exist anyway
+                    NotFound => Ok(()),
+                    // otherwise return the error
+                    _ => Err(Box::new(e)),
+                }
+            }
+        }
+    }
 }
 
 impl From<&configuration::Files> for AlwaysOnFile {
     fn from(files: &configuration::Files) -> Self {
         Self::new(PathBuf::from(files.always_on.clone()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::*;
+    use tempfile::*;
+
+    fn get_path(file: &NamedTempFile) -> PathBuf {
+        PathBuf::from(file.path())
+    }
+
+    #[fixture]
+    fn file() -> NamedTempFile {
+        NamedTempFile::new().unwrap()
+    }
+
+    #[rstest]
+    fn is_always_on_fails_if_file_doesnt_exist(file: NamedTempFile) {
+        let path = get_path(&file);
+        file.close().unwrap();
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(!always_on.is_always_on());
+    }
+
+    #[rstest]
+    fn is_always_on_succeeds_if_file_exists(file: NamedTempFile) {
+        let path = get_path(&file);
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(always_on.is_always_on());
+    }
+
+    #[rstest]
+    fn set_always_on_succeeds_if_file_doesnt_exist(file: NamedTempFile) {
+        let path = get_path(&file);
+        file.close().unwrap();
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(always_on.set_always_on().is_ok());
+    }
+
+    #[rstest]
+    fn set_always_on_succeeds_if_file_exists(file: NamedTempFile) {
+        let path = get_path(&file);
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(always_on.set_always_on().is_ok());
+    }
+
+    #[rstest]
+    fn reset_always_on_succeeds_if_file_doesnt_exist(file: NamedTempFile) {
+        let path = get_path(&file);
+        file.close().unwrap();
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(always_on.reset_always_on().is_ok());
+    }
+
+    #[rstest]
+    fn reset_always_on_succeeds_if_file_exists(file: NamedTempFile) {
+        let path = get_path(&file);
+
+        let always_on = AlwaysOnFile::new(path);
+        assert!(always_on.reset_always_on().is_ok());
     }
 }
